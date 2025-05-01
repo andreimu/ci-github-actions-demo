22038b0ef3b2cdfb213d304125b2c39e8e62f6ee
andreimu <andrei.muzikant@gmail.com>
2025-05-01 15:22:38 +0300

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -8,4 +8,5 @@ class TestMain(unittest.TestCase):
         self.assertEqual(add(0, 0), 0)
 
 if __name__ == "__main__":
-    unittest.main(buffer=False)
+    runner = unittest.TextTestRunner()
+    unittest.main(testRunner=runner)
